70c7da7b99580b4b6ee37f7b5b9a04b3a1a633bc
Jerin <jerin.dev.jobi@gmail.com>
2025-04-18 11:19:57 +0900

    fix: base url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000", // Update with your backend URL
+  baseURL: "https://tapsndr-ticket.ecmrare.com:3000", // Update with your backend URL
   headers: {
     "Content-Type": "application/json",
   },
